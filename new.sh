d0bdc855cd9593dcd6e4153bc677c4497ffa87b9
ratrionil <ratrionil.gmail.com>
2025-05-04 00:59:27 +0530

    Learnt Cron Anacron At

--- new.sh
+++ new.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "welcome to process management trial !"
-mkdir news
-cd news
+
+echo "welcome to process management trial !" >> /home/ratri0kali/KALI/news.txt
+mkdir -p /home/ratri0kali/KALI/news
+cd /home/ratri0kali/KALI/news
+
 for i in {1..5}
 do
-touch file$i
-echo "created file$i" >> ../news.txt
-sleep 3s
+    touch file$i
+    echo "created file$i" >> /home/ratri0kali/KALI/news.txt
+    sleep 3s
 done
-echo "done !!"
-cd ..
+
+echo "done !!" >> /home/ratri0kali/KALI/news.txt
